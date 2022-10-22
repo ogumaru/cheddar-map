df05b51ee89b747fd7bae82b43293c415683efd3
ogumaru <100858200+ogumaru@users.noreply.github.com>
2022-10-23 00:47:12 +0900

    fix: check if root element is null

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,10 @@ import esriConfig from "@arcgis/core/config.js";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-const root = createRoot(document.querySelector("div#app"));
+const appRootSelector = "div#app";
+const appRoot = document.querySelector(appRootSelector);
+if (!appRoot) throw new Error(`error: ${appRootSelector} not found`);
+const root = createRoot(appRoot);
 root.render(<App />);
 
 esriConfig.assetsPath = "./assets";
