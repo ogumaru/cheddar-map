ee2cc6983a8f3667c039e8b769fbf6d00a1f5ce9
ogumaru <100858200+ogumaru@users.noreply.github.com>
2022-10-25 06:57:10 +0900

    update: use calcite block as menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -2,31 +2,46 @@ import { downloadGraphicsAsCSV } from "../../download/graphics";
 import { handleSelection } from "../../loadCSV";
 import { useContext } from "react";
 import { MenuContext } from "../../contexts";
+import {
+  CalciteBlock,
+  CalciteBlockSection,
+  CalciteIcon,
+  CalciteNotice,
+} from "@esri/calcite-components-react";
 
 export const Menu = () => {
   const context = useContext(MenuContext);
   return (
     <>
-      <div id="menu">
-        <label htmlFor="fileselect-csv">Choose CSV</label>
-        <input
-          id="fileselect-csv"
-          type="file"
-          multiple={true}
-          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-          onChange={handleSelection}
-        />
-        <button id="dl-graphics" onClick={() => downloadGraphicsAsCSV()}>
-          Download
-        </button>
-        <label htmlFor="set-attributes">Set Attribute</label>
-        <input
-          id="set-attributes"
-          type="checkbox"
-          checked={context.isSetAttr}
-          onChange={(e) => context.setIsSetAttr(e.target.checked)}
-        />
-      </div>
+      <CalciteBlock collapsible heading="メニュー" style={{ margin: 0 }}>
+        <CalciteIcon scale="m" slot="icon" icon="hamburger"></CalciteIcon>
+        <div id="menu">
+          <CalciteBlockSection text="インポート">
+            <label htmlFor="fileselect-csv">Choose CSV</label>
+            <input
+              id="fileselect-csv"
+              type="file"
+              multiple={true}
+              accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+              onChange={handleSelection}
+            />
+          </CalciteBlockSection>
+          <CalciteBlockSection text="エクスポート">
+            <button id="dl-graphics" onClick={() => downloadGraphicsAsCSV()}>
+              Download
+            </button>
+          </CalciteBlockSection>
+          <CalciteBlockSection text="設定">
+            <label htmlFor="set-attributes">Set Attribute</label>
+            <input
+              id="set-attributes"
+              type="checkbox"
+              checked={context.isSetAttr}
+              onChange={(e) => context.setIsSetAttr(e.target.checked)}
+            />
+          </CalciteBlockSection>
+        </div>
+      </CalciteBlock>
     </>
   );
 };
