173e40c695c3dd1c224b888719da931c08146937
ogumaru <100858200+ogumaru@users.noreply.github.com>
2022-10-30 14:29:55 +0900

    change: not to use calcite-block-section

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -2,12 +2,7 @@ import { downloadGraphicsAsCSV } from "../../download/graphics";
 import { handleSelection } from "../../loadCSV";
 import { useContext } from "react";
 import { MenuContext } from "../../contexts";
-import {
-  CalciteBlock,
-  CalciteBlockSection,
-  CalciteIcon,
-  CalciteNotice,
-} from "@esri/calcite-components-react";
+import { CalciteBlock, CalciteIcon } from "@esri/calcite-components-react";
 
 export const Menu = () => {
   const context = useContext(MenuContext);
@@ -16,30 +11,24 @@ export const Menu = () => {
       <CalciteBlock collapsible heading="メニュー" style={{ margin: 0 }}>
         <CalciteIcon scale="m" slot="icon" icon="hamburger"></CalciteIcon>
         <div id="menu">
-          <CalciteBlockSection text="インポート">
-            <label htmlFor="fileselect-csv">CSVファイル</label>
-            <input
-              id="fileselect-csv"
-              type="file"
-              multiple={true}
-              accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-              onChange={handleSelection}
-            />
-          </CalciteBlockSection>
-          <CalciteBlockSection text="エクスポート">
-            <button id="dl-graphics" onClick={() => downloadGraphicsAsCSV()}>
-              CSVファイル
-            </button>
-          </CalciteBlockSection>
-          <CalciteBlockSection text="設定">
-            <label htmlFor="set-attributes">ポイントへ属性情報を追加</label>
-            <input
-              id="set-attributes"
-              type="checkbox"
-              checked={context.isSetAttr}
-              onChange={(e) => context.setIsSetAttr(e.target.checked)}
-            />
-          </CalciteBlockSection>
+          <label htmlFor="fileselect-csv">CSVファイルのインポート</label>
+          <input
+            id="fileselect-csv"
+            type="file"
+            multiple={true}
+            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+            onChange={handleSelection}
+          />
+          <button id="dl-graphics" onClick={() => downloadGraphicsAsCSV()}>
+            CSVファイルのエクスポート
+          </button>
+          <label htmlFor="set-attributes">ポイントへ属性情報を追加</label>
+          <input
+            id="set-attributes"
+            type="checkbox"
+            checked={context.isSetAttr}
+            onChange={(e) => context.setIsSetAttr(e.target.checked)}
+          />
         </div>
       </CalciteBlock>
     </>
