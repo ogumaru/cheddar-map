d6537b437523d39452c3790ad59318922087a1f3
ogumaru <100858200+ogumaru@users.noreply.github.com>
2022-10-30 16:45:28 +0900

    update: use calcite components

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -2,34 +2,55 @@ import { downloadGraphicsAsCSV } from "../../download/graphics";
 import { handleSelection } from "../../loadCSV";
 import { useContext } from "react";
 import { MenuContext } from "../../contexts";
-import { CalciteBlock, CalciteIcon } from "@esri/calcite-components-react";
+import {
+  CalciteBlock,
+  CalciteIcon,
+  CalciteSwitch,
+  CalciteLabel,
+  CalciteButton,
+  CalcitePanel,
+} from "@esri/calcite-components-react";
 
 export const Menu = () => {
   const context = useContext(MenuContext);
+  const marginEachComponents = "0.5em";
   return (
     <>
       <CalciteBlock collapsible heading="メニュー" style={{ margin: 0 }}>
         <CalciteIcon scale="m" slot="icon" icon="hamburger"></CalciteIcon>
-        <div id="menu">
-          <label htmlFor="fileselect-csv">CSVファイルのインポート</label>
-          <input
-            id="fileselect-csv"
-            type="file"
-            multiple={true}
-            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-            onChange={handleSelection}
-          />
-          <button id="dl-graphics" onClick={() => downloadGraphicsAsCSV()}>
+        <CalcitePanel id="menu">
+          {/* CalciteInputがfileのmultipleをサポートしていないため素のinput */}
+          <div style={{ margin: marginEachComponents }}>
+            <label htmlFor="fileselect-csv">CSVファイルのインポート</label>
+            <input
+              id="fileselect-csv"
+              type="file"
+              multiple={true}
+              accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+              onChange={handleSelection}
+            />
+          </div>
+          <CalciteButton
+            style={{ margin: marginEachComponents }}
+            id="dl-graphics"
+            onClick={() => downloadGraphicsAsCSV()}
+          >
             CSVファイルのエクスポート
-          </button>
-          <label htmlFor="set-attributes">ポイントへ属性情報を追加</label>
-          <input
-            id="set-attributes"
-            type="checkbox"
-            checked={context.isSetAttr}
-            onChange={(e) => context.setIsSetAttr(e.target.checked)}
-          />
-        </div>
+          </CalciteButton>
+          <CalciteLabel
+            layout="inline"
+            style={{ margin: marginEachComponents }}
+          >
+            ポイントへ属性情報を追加
+            <CalciteSwitch
+              id="set-attributes"
+              onCalciteSwitchChange={(e) => {
+                const input = e.target;
+                context.setIsSetAttr(input.checked);
+              }}
+            />
+          </CalciteLabel>
+        </CalcitePanel>
       </CalciteBlock>
     </>
   );
